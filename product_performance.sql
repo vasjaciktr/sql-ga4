ced60cce2c235056dbdfaeadc6773144379df603
Vasyl Tsyktor <vasjaciktr@gmail.com>
2026-03-04 15:58:49 +0200

    Create product_performance.sql
    Top products by date + units + revenue

---
+++ product_performance.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE TABLE `europafoodxb-450709.analytics_ecommerce.product_performance` AS
+WITH purchases AS (
+  SELECT
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'transaction_id') AS transaction_id,
+    items.item_name AS product,
+    items.item_category AS category,
+    items.quantity AS qty,
+    items.price AS price
+  FROM `europafoodxb-450709.analytics_322286584.events_*`,
+  UNNEST(items) AS items
+  WHERE event_name = 'purchase'
+)
+SELECT
+  product,
+  category,
+  SUM(qty) AS units_sold,
+  SUM(qty * price) AS revenue,
+  COUNT(DISTINCT transaction_id) AS orders,
+  SAFE_DIVIDE(SUM(qty * price), COUNT(DISTINCT transaction_id)) AS aov
+FROM purchases
+GROUP BY product, category;
