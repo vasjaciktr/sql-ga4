4e61c8a2e3a01b9efaab2d6d65185930bc07e80d
Vasyl Tsyktor <vasjaciktr@gmail.com>
2026-03-13 18:16:43 +0200

    Update product_сonversion_ecommerce.sql

--- sql/product_сonversion_ecommerce.sql
+++ sql/product_сonversion_ecommerce.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `europafoodxb-450709.analytics_ecommerce.product_conversion_daily`
+CREATE OR REPLACE TABLE `xxxx-450709.analytics_ecommerce.product_conversion_daily`
 PARTITION BY date AS
 
 WITH item_events AS (
