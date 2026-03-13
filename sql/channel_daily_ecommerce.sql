718b52f001bd4aabb280818936ac9b06cfdc67dd
Vasyl Tsyktor <vasjaciktr@gmail.com>
2026-03-13 18:15:35 +0200

    Update channel_daily_ecommerce.sql

--- sql/channel_daily_ecommerce.sql
+++ sql/channel_daily_ecommerce.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `europafoodxb-450709.analytics_ecommerce.channel_daily`
+CREATE OR REPLACE TABLE `xxxx-450709.analytics_ecommerce.channel_daily`
 PARTITION BY date AS
 
 WITH purchases AS (
