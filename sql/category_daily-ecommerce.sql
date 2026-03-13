dccc1f54375a4cc16a5d0627f0c8569c8d1e94e0
Vasyl Tsyktor <vasjaciktr@gmail.com>
2026-03-13 18:15:14 +0200

    Update category_daily-ecommerce.sql

--- sql/category_daily-ecommerce.sql
+++ sql/category_daily-ecommerce.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `europafoodxb-450709.analytics_ecommerce.category_daily`
+CREATE OR REPLACE TABLE `xxxx-450709.analytics_ecommerce.category_daily`
 PARTITION BY date AS
 WITH purchases AS (
   SELECT
